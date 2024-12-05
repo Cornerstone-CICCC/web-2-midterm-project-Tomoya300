6b3cc05c1e386b490355fd8322399fe49e5d5c9c
Tomoya <flyingfox193807@gmail.com>
2024-12-05 10:32:13 -0800

    added TV shows slider and sorted in popularity descendant

--- js/movie.js
+++ js/movie.js
@@ -1,4 +1,4 @@
-const options = {
+const option1 = {
     method: 'GET',
     headers: {
       accept: 'application/json',
@@ -6,82 +6,109 @@ const options = {
     }
   };
   
-  fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
+  fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', option1)
     .then(res => res.json())
     .then(res => {
-
       const movieImage = document.querySelectorAll(".movie_image")
       const movieTitle = document.querySelectorAll(".movie_title")
       const movieDes = document.querySelectorAll(".movie_des")
 
+      const popMovie = res.results.sort((a, b) => b.popularity - a.popularity)
+
       movieImage.forEach((image, index) => {
-          const imageLink = res.results[index].poster_path
+          const imageLink = popMovie[index].poster_path
           image.setAttribute("src", `https://image.tmdb.org/t/p/w300${imageLink}`)
       })
 
       movieTitle.forEach((title, index) => {
-        title.textContent = res.results[index].title
+        title.textContent = popMovie[index].title
       })
 
       movieDes.forEach((des, index) => {
-        des.textContent = res.results[index].overview
+        des.textContent = popMovie[index].overview
       })
-
-      console.log(res)
-    
     })
     .catch(err => console.error(err));
 
-const modalButton = document.querySelectorAll(".modal_button")
-modalButton.forEach((button) => {
-  button.addEventListener("click", function() {
-    console.log("button clicked")
-    const prevOverlay = document.querySelector(".overlay")
-    if (prevOverlay) {
-      prevOverlay.remove()
-    }
-    
-    const modalImage = this.parentElement.querySelector(".movie_image").cloneNode()
-    const modalTitle = this.parentElement.querySelector(".movie_title").cloneNode(true)
-    const modalDes = this.parentElement.querySelector(".movie_des").cloneNode(true)
+const option2 = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTdiNThiODQ1YTY5ZTdlY2IyNmY0OTgxNWZiOTI3MiIsIm5iZiI6MTczMzE2NTAyNi40LCJzdWIiOiI2NzRkZmZlMjU2MmIwMzBiYjVhZGU3MzMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.487BTYd3V5PDtBLIy3XD93cJQdQog7VbivKSEwVf7dk'
+  }
+};
 
-    const overlay = document.createElement("div")
-    const modal = document.createElement("div")
-    const modalRight = document.createElement("div")
-    const closeModal = document.createElement("div")
-    const closeIcon = document.createElement("p")
-    
-    overlay.className = "overlay"
-    modal.className = "modal"
-    modalRight.className = "modal_right"
-    closeModal.className = "close_modal"
-    closeIcon.className = "close_icon"
-    closeIcon.innerHTML = "&#10006;"
+fetch('https://api.themoviedb.org/3/trending/tv/day?language=en-US', option2)
+  .then(res => res.json())
+  .then(res => {
+    const tvImage = document.querySelectorAll(".tv_image")
+    const tvTitle = document.querySelectorAll(".tv_title")
+    const tvDes = document.querySelectorAll(".tv_des")
 
-    modalRight.append(modalTitle, modalDes)
-    closeModal.appendChild(closeIcon)
-    modal.append(modalImage, modalRight, closeModal)
-    overlay.appendChild(modal)
-    document.body.appendChild(overlay)
+    const popTv = res.results.sort((a, b) => b.popularity - a.popularity)
 
-    closeModal.addEventListener("click", function() {
-      overlay.remove()
+    tvImage.forEach((image, index) => {
+        const imageLink = popTv[index].poster_path
+        image.setAttribute("src", `https://image.tmdb.org/t/p/w300${imageLink}`)
     })
-    
-  })
 
-  document.addEventListener("click", function(e) {
-    if (!e.target.closest(".modal")) {
-      document.querySelector(".overlay").remove()
-    }
-  })
+    tvTitle.forEach((title, index) => {
+      title.textContent = popTv[index].name
+    })
 
-
-  document.querySelectorAll(".modal_button").forEach((element) => {
-    element.addEventListener("click", function(e) {
-      e.stopPropagation()
+    tvDes.forEach((des, index) => {
+      des.textContent = popTv[index].overview
     })
   })
-})
+  .catch(err => console.error(err));
 
+  const modalButton = document.querySelectorAll(".modal_button")
+  modalButton.forEach((button) => {
+    button.addEventListener("click", function() {
+      console.log("button clicked")
+      const prevOverlay = document.querySelector(".overlay")
+      if (prevOverlay) {
+        prevOverlay.remove()
+      }
+      const modalImage = this.parentElement.querySelector(".media_image").cloneNode()
+      const modalTitle = this.parentElement.querySelector(".media_title").cloneNode(true)
+      const modalDes = this.parentElement.querySelector(".media_des").cloneNode(true)
+  
+      const overlay = document.createElement("div")
+      const modal = document.createElement("div")
+      const modalRight = document.createElement("div")
+      const closeModal = document.createElement("div")
+      const closeIcon = document.createElement("p")
+      
+      overlay.className = "overlay"
+      modal.className = "modal"
+      modalRight.className = "modal_right"
+      closeModal.className = "close_modal"
+      closeIcon.className = "close_icon"
+      closeIcon.innerHTML = "&#10006;"
+  
+      modalRight.append(modalTitle, modalDes)
+      closeModal.appendChild(closeIcon)
+      modal.append(modalImage, modalRight, closeModal)
+      overlay.appendChild(modal)
+      document.body.appendChild(overlay)
+  
+      closeModal.addEventListener("click", function() {
+        overlay.remove()
+      })
+    })
+  
+    document.addEventListener("click", function(e) {
+      if (document.querySelector(".overlay") && !e.target.closest(".modal")) {
+        document.querySelector(".overlay").remove()
+      }
+    })
+  
+    document.querySelectorAll(".modal_button").forEach((element) => {
+      element.addEventListener("click", function(e) {
+        e.stopPropagation()
+      })
+    })
+  })
+  
 
