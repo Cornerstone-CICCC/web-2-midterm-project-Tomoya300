37478614e4451a3908407249484d57db21c868d6
Tomoya <flyingfox193807@gmail.com>
2024-12-06 12:16:10 -0800

    added slider movement and genre

--- js/movie.js
+++ js/movie.js
@@ -49,6 +49,7 @@ fetch('https://api.themoviedb.org/3/trending/tv/day?language=en-US', tv)
     const tvImage = document.querySelectorAll(".tv_image")
     const tvTitle = document.querySelectorAll(".tv_title")
     const tvDes = document.querySelectorAll(".tv_des")
+    const tvGenre = document.querySelectorAll(".tv_genre")
 
     const popTv = res.results.sort((a, b) => b.popularity - a.popularity)
 
@@ -64,42 +65,14 @@ fetch('https://api.themoviedb.org/3/trending/tv/day?language=en-US', tv)
     tvDes.forEach((des, index) => {
       des.textContent = popTv[index].overview
     })
-  })
-  .catch(err => console.error(err));
 
-const genres = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTdiNThiODQ1YTY5ZTdlY2IyNmY0OTgxNWZiOTI3MiIsIm5iZiI6MTczMzE2NTAyNi40LCJzdWIiOiI2NzRkZmZlMjU2MmIwMzBiYjVhZGU3MzMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.487BTYd3V5PDtBLIy3XD93cJQdQog7VbivKSEwVf7dk'
-  }
-};
-
-fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', genres)
-  .then(res => res.json())
-  .then(res => {
-
-    const genreArr = res.genres
-    const mediaGenre = document.querySelectorAll(".media_genre")
-
-    mediaGenre.forEach((genre) => {
-      const idsString = genre.textContent
-      const stringToNum = idsString.split(",").map(id => parseInt(id.trim()))
-      const numToName = stringToNum.map(id => {
-        const findId = genreArr.find(genre => genre.id === id)
-        return findId ? findId.name : null;
-      })
-      const genreName = numToName.filter(name => name !== null)
-      const newGenre = document.createElement("p")
-      newGenre.classList.add("media_genre", "genre_name")
-      newGenre.textContent = "Genre: " + genreName.join(", ")
-      genre.after(newGenre)
-
+    tvGenre.forEach((genre, index) => {
+      genre.textContent = popTv[index].genre_ids
     })
   })
   .catch(err => console.error(err));
 
-const searchMedia = {
+const genresM = {
   method: 'GET',
   headers: {
     accept: 'application/json',
@@ -107,25 +80,50 @@ const searchMedia = {
   }
 };
 
-const searchQuery = document.querySelector("input").value
-const searchURL = `https://api.themoviedb.org/3/search/multi?include_adult=false&language=en-US&page=1&query=${encodeURIComponent(searchQuery)}`;
-
-fetch(searchURL, searchMedia)
+fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', genresM)
   .then(res => res.json())
   .then(res => {
-    console.log(res);
-    if (res.results.length > 0) {
-      res.results.forEach(result => {
-        console.log(`Title: ${result.title || result.name}`);
-        console.log(`Overview: ${result.overview}`);
-        console.log(`Genres: ${result.genre_ids}`);
-      });
-    } else {
-      console.log('No results found.');
-    }
+
+    const genreMap = new Map(res.genres.map(genre => [genre.id, genre.name]))
+    const movieGenre = document.querySelectorAll(".movie_genre")
+
+    movieGenre.forEach((genre) => {
+      const idsString = genre.textContent
+      const stringToNum = idsString.split(",").map(id => parseInt(id.trim()))
+      const numToName = stringToNum.map(id => genreMap.get(id)).filter(name => name)
+      const newGenre = document.createElement("p")
+      newGenre.classList.add("media_genre", "genre_name")
+      newGenre.textContent = "Genre: " + (numToName.length ? numToName.join(" / ") : "Unknown")
+      genre.after(newGenre)
+    })
   })
   .catch(err => console.error(err));
 
+const genresTv = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTdiNThiODQ1YTY5ZTdlY2IyNmY0OTgxNWZiOTI3MiIsIm5iZiI6MTczMzE2NTAyNi40LCJzdWIiOiI2NzRkZmZlMjU2MmIwMzBiYjVhZGU3MzMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.487BTYd3V5PDtBLIy3XD93cJQdQog7VbivKSEwVf7dk'
+  }
+};
+
+fetch('https://api.themoviedb.org/3/genre/tv/list?language=en', genresTv)
+  .then(res => res.json())
+  .then(res => {
+    const genreMap = new Map(res.genres.map(genre => [genre.id, genre.name]))
+    const tvGenre = document.querySelectorAll(".tv_genre")
+
+    tvGenre.forEach((genre) => {
+      const idsString = genre.textContent
+      const stringToNum = idsString.split(",").map(id => parseInt(id.trim()))
+      const numToName = stringToNum.map(id => genreMap.get(id)).filter(name => name)
+      const newGenre = document.createElement("p")
+      newGenre.classList.add("media_genre", "genre_name")
+      newGenre.textContent = "Genre: " + (numToName.length ? numToName.join(" / ") : "Unknown")
+      genre.after(newGenre)
+    })
+  })
+  .catch(err => console.error(err));
 
   const modalButton = document.querySelectorAll(".modal_button")
   modalButton.forEach((button) => {
@@ -139,6 +137,9 @@ fetch(searchURL, searchMedia)
       const modalTitle = this.parentElement.querySelector(".media_title").cloneNode(true)
       const modalDes = this.parentElement.querySelector(".media_des").cloneNode(true)
       const modalGenre = this.parentElement.querySelector(".genre_name").cloneNode(true)
+
+      modalTitle.classList.add("modal_title")
+      modalImage.classList.add("modal_image")
 
       modalGenre.classList.remove("media_genre")
   
@@ -178,5 +179,113 @@ fetch(searchURL, searchMedia)
       })
     })
   })
+
+  const sliders = document.querySelectorAll('.slider_container'); 
+  const images = document.querySelectorAll('.media_image'); 
+  let startX, scrollLeft, scrollSpeed = 0, lastX, lastTime;
+  
+  sliders.forEach(scrollContainer => {
+    scrollContainer.addEventListener('mousedown', (e) => {
+      isMouseDown = true;
+      startX = e.pageX - scrollContainer.offsetLeft;
+      scrollLeft = scrollContainer.scrollLeft;
+      lastX = e.pageX;
+      lastTime = Date.now();
+      scrollSpeed = 0; 
+      scrollContainer.style.cursor = 'grabbing'; 
+
+      scrollContainer.style.userSelect = 'none'; 
+      
+      images.forEach(image => {
+        image.style.pointerEvents = 'none';
+      });
+    });
+  
+    scrollContainer.addEventListener('mouseleave', () => {
+      isMouseDown = false;
+      scrollContainer.style.cursor = 'grab';
+      scrollContainer.style.userSelect = 'auto'; 
+
+      images.forEach(image => {
+        image.style.pointerEvents = 'auto';
+      });
+
+      inertia(scrollContainer);
+    });
+
+    scrollContainer.addEventListener('mouseup', () => {
+      isMouseDown = false;
+      scrollContainer.style.cursor = 'grab';
+      scrollContainer.style.userSelect = 'auto';  
+
+      images.forEach(image => {
+        image.style.pointerEvents = 'auto';
+      });
+
+      inertia(scrollContainer);
+    });
+
+    scrollContainer.addEventListener('mousemove', (e) => {
+      if (!isMouseDown) return;
+      e.preventDefault();
+      const x = e.pageX - scrollContainer.offsetLeft;
+      const walk = (x - startX) * 2;
+      scrollContainer.scrollLeft = scrollLeft - walk;
+
+      const now = Date.now();
+      const timeDelta = now - lastTime; 
+      const distance = e.pageX - lastX;
+      scrollSpeed = distance / timeDelta;
+      lastX = e.pageX;
+      lastTime = now; 
+    });
+
+    scrollContainer.addEventListener('mouseover', (e) => {
+      if (!isMouseDown && e.target && e.target.nodeType === Node.TEXT_NODE) {
+        scrollContainer.style.userSelect = 'text';
+      } else {
+        scrollContainer.style.userSelect = 'none';
+      }
+    });
+  });
+
+  function inertia(scrollContainer) {
+    if (Math.abs(scrollSpeed) > 0.1) {
+      scrollContainer.scrollLeft -= scrollSpeed * 10;
+      scrollSpeed *= 0.95; 
+      requestAnimationFrame(() => inertia(scrollContainer));
+    }
+  }
   
 
+
+
+
+
+  // const searchMedia = {
+  //   method: 'GET',
+  //   headers: {
+  //     accept: 'application/json',
+  //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTdiNThiODQ1YTY5ZTdlY2IyNmY0OTgxNWZiOTI3MiIsIm5iZiI6MTczMzE2NTAyNi40LCJzdWIiOiI2NzRkZmZlMjU2MmIwMzBiYjVhZGU3MzMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.487BTYd3V5PDtBLIy3XD93cJQdQog7VbivKSEwVf7dk'
+  //   }
+  // };
+  
+  // const searchQuery = document.querySelector("input").value
+  // const searchURL = `https://api.themoviedb.org/3/search/multi?include_adult=false&language=en-US&page=1&query=${encodeURIComponent(searchQuery)}`;
+  
+  // fetch(searchURL, searchMedia)
+  //   .then(res => res.json())
+  //   .then(res => {
+  //     console.log(res);
+  //     if (res.results.length > 0) {
+  //       res.results.forEach(result => {
+  //         console.log(`Title: ${result.title || result.name}`);
+  //         console.log(`Overview: ${result.overview}`);
+  //         console.log(`Genres: ${result.genre_ids}`);
+  //       });
+  //     } else {
+  //       console.log('No results found.');
+  //     }
+  //   })
+  //   .catch(err => console.error(err));
+  
