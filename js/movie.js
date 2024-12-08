0c9b8277c3fe50819442ef4a17adbe1e4d74b448
Tomoya <flyingfox193807@gmail.com>
2024-12-08 14:38:01 -0800

    updated list of movies up to 20 and separated js files

--- js/movie.js
+++ js/movie.js
@@ -17,24 +17,32 @@ const movie = {
       const popMovie = res.results.sort((a, b) => b.popularity - a.popularity)
 
       movieImage.forEach((image, index) => {
+        if (index < res.results.length) {
           const imageLink = popMovie[index].poster_path
           image.setAttribute("src", `https://image.tmdb.org/t/p/w300${imageLink}`)
+        }
       })
 
       movieTitle.forEach((title, index) => {
-        const rank = document.createElement("span")
-        rank.className = "rank"
-        rank.textContent = `${index+1}`
-        title.appendChild(rank)
-        title.appendChild(document.createTextNode(`${popMovie[index].title}`))
+        if (index < res.results.length) {
+          const rank = document.createElement("span")
+          rank.className = "rank"
+          rank.textContent = `${index+1}`
+          title.appendChild(rank)
+          title.appendChild(document.createTextNode(`${popMovie[index].title}`))
+        }
       })
 
       movieDes.forEach((des, index) => {
-        des.textContent = popMovie[index].overview
+        if (index < res.results.length) {
+          des.textContent = popMovie[index].overview
+        }
       })
 
       movieGenre.forEach((genre, index) => {
-        genre.textContent = popMovie[index].genre_ids
+        if (index < res.results.length) {
+          genre.textContent = popMovie[index].genre_ids
+        }
       })
     })
     .catch(err => console.error(err));
@@ -187,103 +195,3 @@ fetch('https://api.themoviedb.org/3/genre/tv/list?language=en', genresTv)
       })
     })
   })
-
-  const sliders = document.querySelectorAll(".slider_container")
-  const images = document.querySelectorAll(".media_image")
-  let isMouseDown = false
-  let startX, scrollLeft, scrollSpeed = 0, lastX, lastTime
-  
-  sliders.forEach(scrollContainer => {
-    scrollContainer.addEventListener("mousedown", (e) => {
-      isMouseDown = true
-      startX = e.pageX - scrollContainer.offsetLeft
-      scrollLeft = scrollContainer.scrollLeft
-      lastX = e.pageX
-      lastTime = Date.now()
-      scrollSpeed = 0
-      scrollContainer.style.userSelect = "none"
-
-      images.forEach(image => {
-        image.style.pointerEvents = "none"
-      })
-    })
-
-    scrollContainer.addEventListener("mouseleave", () => {
-      isMouseDown = false
-      scrollContainer.style.userSelect = "auto"
-
-      images.forEach(image => {
-        image.style.pointerEvents = "auto"
-      })
-
-      inertia(scrollContainer)
-    })
-
-    scrollContainer.addEventListener("mouseup", () => {
-      isMouseDown = false
-      scrollContainer.style.userSelect = "auto"
-
-      images.forEach(image => {
-        image.style.pointerEvents = "auto"
-      })
-
-      inertia(scrollContainer)
-    })
-
-    scrollContainer.addEventListener("mousemove", (e) => {
-      if (!isMouseDown) return
-      e.preventDefault()
-      const x = e.pageX - scrollContainer.offsetLeft
-      const walk = (x - startX) * 2
-      scrollContainer.scrollLeft = scrollLeft - walk
-
-      const now = Date.now()
-      const timeDelta = now - lastTime
-      const distance = e.pageX - lastX
-      scrollSpeed = distance / timeDelta
-      lastX = e.pageX
-      lastTime = now
-    })
-  })
-
-  function inertia(scrollContainer) {
-    if (Math.abs(scrollSpeed) > 0.1) {
-      scrollContainer.scrollLeft -= scrollSpeed * 10
-      scrollSpeed *= 0.95
-      requestAnimationFrame(() => inertia(scrollContainer))
-    }
-  }
-  
-  
-
-
-
-
-
-  // const searchMedia = {
-  //   method: "GET",
-  //   headers: {
-  //     accept: 'application/json',
-  //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTdiNThiODQ1YTY5ZTdlY2IyNmY0OTgxNWZiOTI3MiIsIm5iZiI6MTczMzE2NTAyNi40LCJzdWIiOiI2NzRkZmZlMjU2MmIwMzBiYjVhZGU3MzMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.487BTYd3V5PDtBLIy3XD93cJQdQog7VbivKSEwVf7dk'
-  //   }
-  // };
-  
-  // const searchQuery = document.querySelector("input").value
-  // const searchURL = `https://api.themoviedb.org/3/search/multi?include_adult=false&language=en-US&page=1&query=${encodeURIComponent(searchQuery)}`;
-  
-  // fetch(searchURL, searchMedia)
-  //   .then(res => res.json())
-  //   .then(res => {
-  //     console.log(res);
-  //     if (res.results.length > 0) {
-  //       res.results.forEach(result => {
-  //         console.log(`Title: ${result.title || result.name}`);
-  //         console.log(`Overview: ${result.overview}`);
-  //         console.log(`Genres: ${result.genre_ids}`);
-  //       });
-  //     } else {
-  //       console.log('No results found.');
-  //     }
-  //   })
-  //   .catch(err => console.error(err));
-  
